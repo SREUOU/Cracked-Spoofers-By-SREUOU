a4152d927301ff5d0d0a06c60f2f327701ce4338
SREUOU <97848439+SREUOU@users.noreply.github.com>
2022-12-01 21:45:26 +0200

    EASY:)

---
+++ Cracked Lean FiveM Spoofer/Lean Spoof fivem/KeyAuth/encryption.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace KeyAuth
+{
+	// Token: 0x02000003 RID: 3
+	public static class encryption
+	{
+		// Token: 0x0600001B RID: 27 RVA: 0x00003748 File Offset: 0x00001948
+		public static string byte_arr_to_str(byte[] ba)
+		{
+			StringBuilder stringBuilder = new StringBuilder(ba.Length * 2);
+			foreach (byte b in ba)
+			{
+				stringBuilder.AppendFormat("{0:x2}", b);
+			}
+			return stringBuilder.ToString();
+		}
+
+		// Token: 0x0600001C RID: 28 RVA: 0x00003794 File Offset: 0x00001994
+		public static byte[] str_to_byte_arr(string hex)
+		{
+			byte[] result;
+			try
+			{
+				int length = hex.Length;
+				byte[] array = new byte[length / 2];
+				for (int i = 0; i < length; i += 2)
+				{
+					array[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+				}
+				result = array;
+			}
+			catch
+			{
+				Console.WriteLine("\n\n  The session has ended, open program again.");
+				Thread.Sleep(3500);
+				Environment.Exit(0);
+				result = null;
+			}
+			return result;
+		}
+
+		// Token: 0x0600001D RID: 29 RVA: 0x00003814 File Offset: 0x00001A14
+		public static string encrypt_string(string plain_text, byte[] key, byte[] iv)
+		{
+			Aes aes = Aes.Create();
+			aes.Mode = CipherMode.CBC;
+			aes.Key = key;
+			aes.IV = iv;
+			string result;
+			using (MemoryStream memoryStream = new MemoryStream())
+			{
+				using (ICryptoTransform cryptoTransform = aes.CreateEncryptor())
+				{
+					using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+					{
+						byte[] bytes = Encoding.Default.GetBytes(plain_text);
+						cryptoStream.Write(bytes, 0, bytes.Length);
+						cryptoStream.FlushFinalBlock();
+						byte[] ba = memoryStream.ToArray();
+						result = encryption.byte_arr_to_str(ba);
+					}
+				}
+			}
+			return result;
+		}
+
+		// Token: 0x0600001E RID: 30 RVA: 0x000038DC File Offset: 0x00001ADC
+		public static string decrypt_string(string cipher_text, byte[] key, byte[] iv)
+		{
+			Aes aes = Aes.Create();
+			aes.Mode = CipherMode.CBC;
+			aes.Key = key;
+			aes.IV = iv;
+			string @string;
+			using (MemoryStream memoryStream = new MemoryStream())
+			{
+				using (ICryptoTransform cryptoTransform = aes.CreateDecryptor())
+				{
+					using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+					{
+						byte[] array = encryption.str_to_byte_arr(cipher_text);
+						cryptoStream.Write(array, 0, array.Length);
+						cryptoStream.FlushFinalBlock();
+						byte[] array2 = memoryStream.ToArray();
+						@string = Encoding.Default.GetString(array2, 0, array2.Length);
+					}
+				}
+			}
+			return @string;
+		}
+
+		// Token: 0x0600001F RID: 31 RVA: 0x000039A8 File Offset: 0x00001BA8
+		public static string iv_key()
+		{
+			return Guid.NewGuid().ToString().Substring(0, Guid.NewGuid().ToString().IndexOf("-", StringComparison.Ordinal));
+		}
+
+		// Token: 0x06000020 RID: 32 RVA: 0x000039EC File Offset: 0x00001BEC
+		public static string sha256(string r)
+		{
+			return encryption.byte_arr_to_str(new SHA256Managed().ComputeHash(Encoding.Default.GetBytes(r)));
+		}
+
+		// Token: 0x06000021 RID: 33 RVA: 0x00003A08 File Offset: 0x00001C08
+		public static string encrypt(string message, string enc_key, string iv)
+		{
+			byte[] bytes = Encoding.Default.GetBytes(encryption.sha256(enc_key).Substring(0, 32));
+			byte[] bytes2 = Encoding.Default.GetBytes(encryption.sha256(iv).Substring(0, 16));
+			return encryption.encrypt_string(message, bytes, bytes2);
+		}
+
+		// Token: 0x06000022 RID: 34 RVA: 0x00003A54 File Offset: 0x00001C54
+		public static string decrypt(string message, string enc_key, string iv)
+		{
+			byte[] bytes = Encoding.Default.GetBytes(encryption.sha256(enc_key).Substring(0, 32));
+			byte[] bytes2 = Encoding.Default.GetBytes(encryption.sha256(iv).Substring(0, 16));
+			return encryption.decrypt_string(message, bytes, bytes2);
+		}
+	}
+}
---
+++ Cracked Lean FiveM Spoofer/Lean Spoof fivem/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Spoof fivem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("8734-16615")]
+[assembly: AssemblyProduct("Spoof fivem")]
+[assembly: AssemblyCopyright("Copyright © 8734-16615 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("28f5afdb-b466-40b2-8aac-033bdc2d47ec")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
